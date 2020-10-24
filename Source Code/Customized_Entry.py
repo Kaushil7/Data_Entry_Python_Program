7c9edbfcd6b1652350bcdff525ea1a378b3c3a9f
Kaushil7 <69051817+Kaushil7@users.noreply.github.com>
2020-10-24 16:58:39 +0530

    Updated

--- Source Code/Customized_Entry.py
+++ Source Code/Customized_Entry.py
@@ -1,40 +1,47 @@
 #importing openpxyl to read excel files
 from openpyxl import load_workbook
-print('*************NOTE ENTER DATA FROM FIRST ROW *************\nMADE BY KAUSHIL NAGRALE\n')
+print('MADE BY KAUSHIL NAGRALE\n')
 print('*************EASY PWD (https://ams.emahapwd.com/ams/login.do entry) *************\n')
-f= open("COPY_THIS_FILE_TO_WEBSITE_JS.txt","w+")
+print('*************NOTE ENTER DATA FROM FIRST ROW *************\n')
+f= open("javaSc.txt","w+")
+print('Keep the File inside the same directory as this .py file\n')
 
+#Taking the details of Excel file as inputs
+shi=input('1. Enter the File name (Excel in file in which data is present)\n')
+bo=input('2. Enter the worksheet name\n')
+y=input('3. Enter the no of rows\n')
+count=1
+count=int(count)
 
-y=input('*************Enter the no of rows*************\n')
-count=int(1)
-
-
-# data based adding of rows
+#Data-based adding of rows
 y=int(y)
 while count < y:
      f.write('addRows();\n')
      count=count+1
 
 
-#Creating list to store the excel data
-re,nu,le,br,de=[],[],[],[],[]
-wb = load_workbook("auto.xlsx")
-ws = wb.get_sheet_by_name("auto")
-column = ws['A']
-re = [column[a].value for a in range(len(column))]
-column = ws['B']
-nu = [column[b].value for b in range(len(column))]
-column = ws['C']
-le = [column[c].value for c in range(len(column))]
-column = ws['D']
-br = [column[d].value for d in range(len(column))]
-column = ws['E']
-de = [column[e].value for e in range(len(column))]
-
-
 #counting and iteration variable
 x = 1
 
+#Creating list to store the excel data
+re,nu,le,br,de=[],[],[],[],[]
+wb = load_workbook(shi+".xlsx")
+ws = wb.get_sheet_by_name(bo)
+ha = input('1. Enter the Item Measurement column\n')
+column = ws[ha]
+re = [column[a].value for a in range(len(column))]
+hb = input('2. Enter the No. column\n')
+column = ws[hb]  # Column
+nu = [column[b].value for b in range(len(column))]
+hc = input('3. Enter the Length column\n')
+column = ws[hc]  # Column
+le = [column[c].value for c in range(len(column))]
+hd = input('4. Enter the Breadth column\n')
+column = ws[hd]  # Column
+br = [column[d].value for d in range(len(column))]
+he = input('5. Enter the depth column\n') # Work Sheet
+column = ws[he]  # Column
+de = [column[e].value for e in range(len(column))]
 
 #Loop for to print the Javascript Command to insert data in rows
 try:
@@ -55,12 +62,11 @@ try:
       x=x+1
 except:
     print("Not enough data")
-
-
-#Loop to print the Javascript command to calculate the quantity on the website
+  
+#Loop for to print the Javascript Command to insert data in rows
 count=1
-while count <=y:
+while count <= y:
     f.write('calculateQty(%d);\n'%count)
     count=count+1
-     
-print('Sucessful!! \n copy the contents of the file of .txt to javascript console')     
+    
+print('Sucessful!! \n copy the contents of the file of .txt to javascript console')
