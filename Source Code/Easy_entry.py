0254a6570b09484a726a16cd9f3f0aa459889361
Kaushil7 <69051817+Kaushil7@users.noreply.github.com>
2020-10-24 16:57:53 +0530

    Updated

--- Source Code/Easy_entry.py
+++ Source Code/Easy_entry.py
@@ -1,56 +1,44 @@
+#importing openpxyl to read excel files
 from openpyxl import load_workbook
 print('*************NOTE ENTER DATA FROM FIRST ROW *************\nMADE BY KAUSHIL NAGRALE\n')
 print('*************EASY PWD (https://ams.emahapwd.com/ams/login.do entry) *************\n')
-f= open("COPY_THIS_FILE_TO_WEBSITE_DATA.txt","w+")
+f= open("COPY_THIS_FILE_TO_WEBSITE_JS.txt","w+")
 
 
 y=input('*************Enter the no of rows*************\n')
-count=1
-count=int(count)
+count=int(1)
+
+
+#Data-based adding of rows
 y=int(y)
 while count < y:
      f.write('addRows();\n')
      count=count+1
 
-cou=y+1
-x = 1
 
-re=[]
+#Creating list to store the excel data
+re,nu,le,br,de=[],[],[],[],[]
 wb = load_workbook("auto.xlsx")
 ws = wb.get_sheet_by_name("auto")
 column = ws['A']
 re = [column[a].value for a in range(len(column))]
-
-nu=[]
-wb = load_workbook("auto.xlsx")
-ws = wb.get_sheet_by_name("auto")
-
 column = ws['B']
 nu = [column[b].value for b in range(len(column))]
-
-le=[]
-wb = load_workbook("auto.xlsx")
-ws = wb.get_sheet_by_name("auto")
-
 column = ws['C']
 le = [column[c].value for c in range(len(column))]
-
-br=[]
-wb = load_workbook("auto.xlsx")
-ws = wb.get_sheet_by_name("auto")
-
 column = ws['D']
 br = [column[d].value for d in range(len(column))]
-
-de=[]
-wb = load_workbook("auto.xlsx")
-ws = wb.get_sheet_by_name("auto")
 column = ws['E']
 de = [column[e].value for e in range(len(column))]
 
-cou=int(cou)
+
+#counting and iteration variable
+x = 1
+
+
+#Loop for to print the Javascript Command to insert data in rows
 try:
-    while x < cou:
+    while x <= y:
       row = ('''document.getElementById('remarks%d').value = '%s';\n''' %(x,re[x-1]))
       if re[x-1]  != None:
            f.write(row)
@@ -68,7 +56,9 @@ try:
 except:
     print("Not enough data")
 
+
+#Loop to print the Javascript command to calculate the quantity on the website
 count=1
-while count < cou:
+while count <=y:
     f.write('calculateQty(%d);\n'%count)
     count=count+1
